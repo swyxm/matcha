843145907443d47ecd69fb5c9ea0c81bf2633e40
swyxm <swayamparekh1276@gmail.com>
2025-09-26 00:04:34 -0700

    initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AnimatePresence } from 'framer-motion';
+import { ThemeProvider } from './context/ThemeContext';
+import { GameProvider, useGame } from './context/GameContext';
+import { AudioProvider } from './context/AudioContext';
+import CassettePlayer from './components/CassettePlayer';
+import IntroStep from './pages/IntroStep';
+import VibeSelectionStep from './pages/VibeSelectionStep';
+import ScoopStep from './pages/ScoopStep';
+import PourStep from './pages/PourStep';
+import WhiskStep from './pages/WhiskStep';
+import ServeStep from './pages/ServeStep';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider>
+      <GameProvider>
+        <AudioProvider>
+          <AppContent />
+        </AudioProvider>
+      </GameProvider>
+    </ThemeProvider>
+  );
+};
 
-export default App
+const AppContent: React.FC = () => {
+  const { gameState } = useGame();
+  
+  const renderStep = () => {
+    switch (gameState.currentStep) {
+      case 'intro':
+        return <IntroStep key="intro" />;
+      case 'choose-vibe':
+        return <VibeSelectionStep key="choose-vibe" />;
+      case 'scoop':
+        return <ScoopStep key="scoop" />;
+      case 'pour':
+        return <PourStep key="pour" />;
+      case 'whisk':
+        return <WhiskStep key="whisk" />;
+      case 'serve':
+        return <ServeStep key="serve" />;
+      case 'certificate':
+        return <div key="certificate">Certificate (Coming Soon)</div>;
+      default:
+        return <IntroStep key="default-intro" />;
+    }
+  };
+  return (
+    <div className="app">
+      <AnimatePresence mode="wait">
+        {renderStep()}
+      </AnimatePresence>
+      <CassettePlayer />
+      <style>
+        {`
+          .app {
+            min-height: 50vh;
+            display: flex;
+            flex-direction: column;
+            background-color: var(--background-color);
+            color: var(--text-color);
+            font-family: var(--font-secondary);
+            line-height: 1.6;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default App;
